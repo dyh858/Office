be03a2d25d3c69c180d1bed45160852ae09d9163
dyh858 <dyh858@163.com>
2015-10-29 11:12:14 +0800

    Simple print

--- Hello.java
+++ Hello.java
@@ -1,11 +1,6 @@
 public class Hello{
-	public static String COMPANY_NAME="dyh858" ;
 	public static void main(String []args){
-		System.out.println("Hello world .");
-		System.out.println("Nice to meet you !");
-		System.out.println("Glad to meet you !");
-	}
-	public static void print() {
-		System.out.println("* Hello World .  *") ;
+		System.out.println("*************");
+		System.out.println("#############");
 	}
 }
