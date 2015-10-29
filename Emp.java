8643b98ef975cbda24aead2c234d100a52cb6e4e
dyh858 <dyh858@163.com>
2015-10-29 08:40:32 +0800

    Add new file Emp.java

---
+++ Emp.java
@@ -0,0 +1,10 @@
+import java.io.* ;
+import java.util.* ;
+public class Emp implements Serializable {
+	private Integer empno ;
+	private String ename ;
+	private String job ;
+	private Double sal ;
+	private Double comm ;
+	private Date hiredate ;
+}
