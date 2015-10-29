d97ed7ee93171f5dce7e7d5373af05a752658e15
dyh858 <dyh858@163.com>
2015-10-29 11:37:56 +0800

    url print

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String []args){
-		System.out.println("*************");
-		System.out.println("#############");
+		System.out.println("www.dyh858.com");
+		System.out.println("www.yootk.com");
 	}
 }
